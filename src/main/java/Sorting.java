db77b3cdaa3ec804d38b9963d2daabaf3f51f49b
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-29 22:25:12 -0500

    Function: Created function for sorting a list using bubble sort

--- src/main/java/Sorting.java
+++ src/main/java/Sorting.java
@@ -112,5 +112,26 @@ public class Sorting {
         while (j < n2) arr[k++] = R[j++];
 		
 	}//end of merge method 
+	
+	public static void bubbleSort(int array[]) {
+		
+		int size = array.length;
+
+		//loop to access each array element
+		for (int i = 0; i < size - 1; i++) {
+			
+			for(int j = 0; j < size-i-1; j++) {
+				
+				if (array[j] > array[j + 1]) {
+					
+					int temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+					
+				}//end of if statement
+				
+			}//end of inner for loop 
+		}//end of outer for loop
+	}//end of bubble sort method
 
 }//end of Sorting class 
