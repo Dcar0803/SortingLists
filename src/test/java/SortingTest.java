39b72ec43bad074bd3ad23e00ad4bb1ef5bb98c3
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-29 21:32:30 -0500

    Test:Created test function for the quick sort function for a list

---
+++ src/test/java/SortingTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SortingTest {
+
+	@Test
+	void testQuickSort() {
+		int[] array = {4,5,3,1,2};
+		int[] expected = {1,2,3,4,5};
+		Sorting.quickSort(array,0, array.length-1);
+		assertArrayEquals(expected, array);
+	}//end of testQuickSort
+	
+
+}//end of SortingTest class 
